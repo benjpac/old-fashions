6fdc20a0315a647acc7eb281c78bf60088b1a80e
Ben Metzger and Ilene Gorski <ben+ilene@>
2017-07-10 12:03:25 -0700

    add user model using devise

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'bootstrap_form'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem "paperclip", "~> 5.0.0"
 gem 'devise'
+gem 'faker'
+gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'carts/show'
   #
   # get 'order_items/create'
   #
   # get 'products/index'
 
+  root 'products#index'
+
   resource :cart, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products do
-    resources :order_items
-  end
+
+  resources :products
+
+  resources :order_items
 
 
 end
