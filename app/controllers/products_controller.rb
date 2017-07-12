8a845c05db7e7ab6c4b1f7954d7f9c2a6edeae8d
Ben Metzger and Ilene Gorski <ben+ilene@>
2017-07-12 16:47:05 -0700

    adding AJAX functionality to product display.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,12 @@ class ProductsController < ApplicationController
     @products = Product.all
     @order_product = current_order.order_products.new
   end
+
+  def show
+    @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to product_path(@product) }
+      format.js { }
+    end
+  end
 end
