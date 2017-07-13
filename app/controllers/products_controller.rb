fb627cafaaeb81870ebb7c8de83da833f817469f
Ben Metzger and Ilene Gorski <ben+ilene@>
2017-07-13 10:36:30 -0700

    add descending direction to sort method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
+  helper_method :sort_default, :direction_default
   def index
-    @products = Product.search(params[:search]).all.paginate(:per_page => 10, :page => params[:page])
+    @products = Product.search(params[:search]).order(sort_default+ " " + direction_default).paginate(:per_page => 10, :page => params[:page])
     @order_product = current_order.order_products.new
   end
 
@@ -11,4 +12,14 @@ class ProductsController < ApplicationController
       format.js { }
     end
   end
+end
+
+private
+
+def sort_default
+  params[:sort] || 'name'
+end
+
+def direction_default
+  params[:direction] || 'asc'
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def sortable(attribute)
+    direction = attribute == sort_default && direction_default == "asc" ? "desc" : "asc"
+    link_to attribute, :sort => attribute, :direction => direction
+  end
 end
